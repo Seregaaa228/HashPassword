c9ceb8a5b89ed60c62e5eea58dd13dee61625b45
skeka <shreker240@gmail.com>
2021-11-20 15:33:30 +0200

    update

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ class PasswordHandler:
     def hash_password_raw(self, password: str) -> str:
         return base64.b64encode(hashlib.sha256(password.encode("utf-8")).digest()).decode("utf-8")
 
-    def hash_password(self, password: str, peper="") -> str:
+    def hash_password(self, password: str, pepper="") -> str:
         salt = base64.b64encode(random.randint(0, 2 ** 256 - 1).to_bytes(32, "little")).decode("utf-8")
-        return self.hash_password_raw(password + ":" + salt + ":" + peper) + ":" + salt + ":" + peper
+        return self.hash_password_raw(password + ":" + salt + ":" + pepper) + ":" + salt + ":" + pepper
 
     def passwords_equal(self, password: str, hash_password: str) -> bool:
-        raw_hash, salt, peper = hash_password.split(":", 3)
-        return self.hash_password(password + ":" + salt + ":" + peper) == raw_hash
+        raw_hash, salt, pepper = hash_password.split(":", 3)
+        return self.hash_password(password + ":" + salt + ":" + pepper) == raw_hash
 
 
 password1 = PasswordHandler()
